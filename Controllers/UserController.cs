9884b1a0a062fcb3d82da662029673fc25e22cf8
mharikmert <mhilmiarikmert@gmail.com>
2021-08-02 16:18:16 +0300

    Implement controller actions

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,13 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using dotnet_jwt_authentication.Models;
+using dotnet_jwt_authentication.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_jwt_authentication.Controllers
 {
-    public class UserController : Controller
+    [ApiController]
+    [Route("api/users")]
+    public class UserController : ControllerBase
     {
-        // GET
-        public IActionResult Index()
+        private IUserService _userService;
+
+        public UserController(IUserService userService)
         {
-            return View();
+            _userService = userService;
+
         }
+        // GET /users
+        [HttpGet]
+        public ActionResult<List<User>> GetAllUsers()
+        {
+            return Ok(_userService.GetAllUsers());
+        }
+
+        //GET /users/{id}
+        [HttpGet("{id}")]
+        public ActionResult<User> GetUser(int id)
+        {
+            var user = _userService.GetUserById(id);
+            if (user is null)
+                return NotFound();
+            return Ok(user);
+        }
+        
+        //POST /users 
+        [HttpPost]
+        public ActionResult<User> CreateUser([FromBody] User user)
+        {
+            return _userService.CreateUser(user);
+        }
+        
+        //DELETE /users/{id}
+        [HttpDelete("{id}")]
+        public void DeleteUser(int id)
+        {
+            _userService.DeleteUser(id);
+        }
+        
     }
 }
